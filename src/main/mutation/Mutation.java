cc5a00f2efd96f92e8207305eee3044597e634d5
floumitou <florian.herzog1998@googlemail.com>
2019-01-08 22:28:26 +0100

    implement random functions
    Implemented random functions to give a random index of the array and a random range of the array

--- src/main/mutation/Mutation.java
+++ src/main/mutation/Mutation.java
@@ -2,20 +2,44 @@ package mutation;
 
 import base.Knapsack;
 import base.Item;
+import random.MersenneTwisterFast;
 import java.util.ArrayList;
 
 public abstract class Mutation {
     public abstract void doMutation(Knapsack knapsack);
+    MersenneTwisterFast rdm = new MersenneTwisterFast();
 
     public String toString() {
         return getClass().getSimpleName();
     }
 
-    protected int getRandomNumber(Knapsack knapsack){
+    //Gibt zufälligen Index der ArrayList mit items aus
+    protected int getRandomIndex(Knapsack knapsack){
         ArrayList<Item> items = knapsack.getItems();
-        int num=0;
         int len = items.size();
+        return rdm.nextInt(0,len);
+    }
 
-        return num;
+    //Gibt eine zufällige Range der ArrayList mit items aus
+    //Hierbei ist die erste Zahl des return-Arrays kleiner als die 2.
+    protected int[] getRandomRange(Knapsack knapsack){
+        int[] tempArray = new int[2];
+        ArrayList<Item> items = knapsack.getItems();
+        int len = items.size();
+        int firstRdm = rdm.nextInt(0,len);
+        int secondRdm;
+        do{
+            secondRdm = rdm.nextInt(0,len);
+        }
+        while(firstRdm == secondRdm);
+        if(firstRdm<secondRdm){
+            tempArray[0] = firstRdm;
+            tempArray[1] = secondRdm;
+        }
+        else{
+            tempArray[0] = secondRdm;
+            tempArray[1]= firstRdm;
+        }
+        return tempArray;
     }
 }
