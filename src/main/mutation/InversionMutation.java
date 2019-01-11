1acdd8b175cc8234e2cbbfadd4c58a17c75e5651
floumitou <florian.herzog1998@googlemail.com>
2019-01-11 19:07:44 +0100

    implemented InversionMutation

--- src/main/mutation/InversionMutation.java
+++ src/main/mutation/InversionMutation.java
@@ -1,28 +1,30 @@
-package main.mutation;
+package mutation;
 
-import main.base.Knapsack;
-import main.base.Item;
+import base.Knapsack;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 // INVM
 public class InversionMutation extends Mutation {
 
     public void doMutation(Knapsack knapsack) {
-        ArrayList<Integer> values = getValueList(knapsack);
+        ArrayList<Integer> values = knapsack.getGenelList();
         ArrayList<Integer> valuesNew = new ArrayList<Integer>();
         int[] range = getRandomRange(knapsack);
         int i = 0;
+
         while(i<range[0]){
             valuesNew.add(values.get(i));
             i++;
         }
-        while(i>=range[1]){
-            valuesNew.add(values.get(range[2]-range[1]));
+
+        int temp = i;
+        while(i>=range[0] && i<=range[1]){
+            valuesNew.add(values.get(range[2]+temp));
             range[2]--;
             i++;
         }
+
         while(i<values.size()){
             valuesNew.add(values.get(i));
             i++;
