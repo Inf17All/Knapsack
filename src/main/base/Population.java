862ac435e8c8ff912105c8790e897186f17bab0d
DeniqueNumquam <43638100+DeniqueNumquam@users.noreply.github.com>
2019-03-02 21:07:02 +0100

    First try, pretty sure its false

--- src/main/base/Population.java
+++ src/main/base/Population.java
@@ -4,4 +4,9 @@ import java.util.ArrayList;
 
 public class Population {
     private ArrayList<Knapsack> knapsackList;
+
+
+    public ArrayList<Knapsack> getKnapsackList(){
+        return knapsackList;
+    }
 }
