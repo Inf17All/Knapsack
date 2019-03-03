ffe523d7801e7c8d4be0f438ea8cbe643845b670
Niklas Koopmann <koopmann.niklas@gmail.com>
2019-03-03 19:41:12 +0100

    little bit of Boltzmann and rank selection

--- src/main/selection/BoltzmannSelection.java
+++ src/main/selection/BoltzmannSelection.java
@@ -2,12 +2,42 @@ package selection;
 
 import base.Knapsack;
 import base.Population;
+import random.MersenneTwisterFast;
 
 import java.util.ArrayList;
 
 // BS
 public class BoltzmannSelection extends Selection {
+
+    private final double kB = 1.38064852e-23; // joule per kelvin
+
     public ArrayList<Knapsack> doSelection(Population population) {
-        return null;
+
+        /*
+
+        What's my temperature? Total fitness of population?
+        What's my energy? Selection (1) or death (0)? Fitness?
+
+         */
+
+        ArrayList<Knapsack> knapsacks = population.getKnapsackList();
+
+        MersenneTwisterFast mtf = new MersenneTwisterFast(System.nanoTime());
+
+        // formula: p(e) = exp(-e/(kB*T))
+
+        int temperature = knapsacks.stream().mapToInt(Knapsack::getTotal).sum(); // T: total fitness of pop
+
+        for (Knapsack k : knapsacks) {
+
+            int knapsackIndex = knapsacks.indexOf(k);
+
+            double pe = Math.exp(-k.getTotal() / (kB * temperature));
+
+            // remove if true because formula spits out high probabilities for less fit individuals
+            if(mtf.nextBoolean(pe)) knapsacks.remove(k);
+        }
+
+        return knapsacks;
     }
 }
--- src/main/selection/RankSelection.java
+++ src/main/selection/RankSelection.java
@@ -2,12 +2,33 @@ package selection;
 
 import base.Knapsack;
 import base.Population;
+import random.MersenneTwisterFast;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 // RS
 public class RankSelection extends Selection {
+
     public ArrayList<Knapsack> doSelection(Population population) {
-        return null;
+
+        MersenneTwisterFast mtf = new MersenneTwisterFast(System.nanoTime());
+        ArrayList<Knapsack> knapsackList = population.getKnapsackList(); // unsorted knapsacks
+
+        knapsackList.sort(Comparator.comparingInt(Knapsack::getTotal).reversed()); // sort knapsacks (ascending fitness)
+
+        int totalRanks = 0; // sum of the ranks to calculate probabilities
+
+        for (int i = 0; i < knapsackList.size(); i++) totalRanks += i + 1; // add up ranks
+
+        for (Knapsack k : knapsackList) { // todo: remove optimal amout of elements!
+
+            int knapsackIndex = knapsackList.indexOf(k);
+
+            if(!mtf.nextBoolean((knapsackIndex * 1.0) / totalRanks)) knapsackList.remove(k);
+        }
+
+        return knapsackList;
     }
 }
